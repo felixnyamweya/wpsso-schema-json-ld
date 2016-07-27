74fb0da3e697f738e1a29f85eead91d3c8621084
JS Morisset <jsm@surniaulula.com>
2016-07-27 11:29:42 -0400

    v1.10.0-dev2

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -12,7 +12,7 @@
  * Description: WPSSO extension to add complete Schema JSON-LD markup (BlogPosting, Article, Place, Product, etc.) for Google and Pinterest.
  * Requires At Least: 3.1
  * Tested Up To: 4.5.3
- * Version: 1.10.0-dev1
+ * Version: 1.10.0-dev2
  * 
  * Version Numbers: {major}.{minor}.{bugfix}-{stage}{level}
  *
@@ -38,7 +38,7 @@ if ( ! class_exists( 'WpssoJson' ) ) {
 		private static $instance = null;
 		private static $req_short = 'WPSSO';
 		private static $req_name = 'WordPress Social Sharing Optimization (WPSSO)';
-		private static $req_min_version = '3.33.8-dev1';
+		private static $req_min_version = '3.33.8-dev2';
 		private static $req_has_min_ver = true;
 
 		public static function &get_instance() {
