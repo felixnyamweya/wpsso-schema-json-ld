7ca2ff297b5c973af9e08a6daf1916de2dd13a7f
JS Morisset <jsm@surniaulula.com>
2016-07-29 08:50:42 -0400

    v1.10.0-1

--- lib/config.php
+++ lib/config.php
@@ -15,8 +15,8 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssojson' => array(
-					'version' => '1.10.0-b2',		// plugin version
-					'opt_version' => '7',		// increment when changing default options
+					'version' => '1.10.0-1',		// plugin version
+					'opt_version' => '8',		// increment when changing default options
 					'short' => 'WPSSO JSON',	// short plugin name
 					'name' => 'WPSSO Schema JSON-LD Markup (WPSSO JSON)',
 					'desc' => 'WPSSO extension to add complete Schema JSON-LD markup (BlogPosting, Article, Organization, Place, Product, etc.) for Google and Pinterest.',
