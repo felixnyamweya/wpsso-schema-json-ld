293dce630625e5032185f0817beaa190633f2caa
JS Morisset <jsm@surniaulula.com>
2016-10-27 10:21:20 -0700

    v1.10.7-dev1

--- lib/gpl/admin/post.php
+++ lib/gpl/admin/post.php
@@ -198,7 +198,7 @@ if ( ! class_exists( 'WpssoJsonGplAdminPost' ) ) {
 
 			$table_rows = $form->get_md_form_rows( $table_rows, $form_rows, $head, $mod, $auto_draft_msg );
 
-			return SucomUtil::after_key( $table_rows, 'subsection_schema',
+			return SucomUtil::get_after_key( $table_rows, 'subsection_schema',
 				'', '<td colspan="2">'.$this->p->msgs->get( 'pro-feature-msg', 
 					array( 'lca' => 'wpssojson' ) ).'</td>' );
 		}
