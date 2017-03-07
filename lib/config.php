dcefb87d5e4a04bd43a7fa6a27cc20d028201f04
JS Morisset <jsm@surniaulula.com>
2017-03-07 02:00:24 -0800

    v1.13.1-dev2

--- lib/config.php
+++ lib/config.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssojson' => array(
-					'version' => '1.13.1-dev1',	// plugin version
+					'version' => '1.13.1-dev2',	// plugin version
 					'opt_version' => '8',		// increment when changing default options
 					'short' => 'WPSSO JSON',	// short plugin name
 					'name' => 'WPSSO Schema JSON-LD Markup (WPSSO JSON)',
@@ -28,7 +28,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.40.2-dev1',
+						'min_version' => '3.40.2-dev2',
 					),
 					'img' => array(
 						'icon_small' => 'images/icon-128x128.png',
