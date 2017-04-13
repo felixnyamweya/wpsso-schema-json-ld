c47fbc12f11ebe536339fd5030dd9bbeb9f82e15
JS Morisset <jsm@surniaulula.com>
2017-04-13 09:12:40 +0200

    v1.13.7-b.1

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to add Schema JSON-LD / SEO markup for Articles, Events, Local Business, Products, Recipes, Reviews + many more.
  * Requires At Least: 3.7
  * Tested Up To: 4.7.3
- * Version: 1.13.7-a.2
+ * Version: 1.13.7-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
