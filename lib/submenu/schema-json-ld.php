177f57992cdac5448388277e9f83e9bb072b6e46
JS Morisset <jsm@surniaulula.com>
2017-08-26 16:34:25 -0400

    v1.16.2-dev.2

--- lib/submenu/schema-json-ld.php
+++ lib/submenu/schema-json-ld.php
@@ -36,8 +36,8 @@ if ( ! class_exists( 'WpssoJsonSubmenuSchemaJsonLd' ) && class_exists( 'WpssoAdm
 			$lca = $this->p->cf['lca'];
 			$metabox = 'schema_json_ld';
 			$tabs = apply_filters( $lca.'_'.$metabox.'_tabs', array( 
-				'general' => _x( 'General Settings', 'metabox tabs', 'wpsso-schema-json-ld' ),
-				'types' => _x( 'Schema Types', 'metabox tabs', 'wpsso-schema-json-ld' ),
+				'general' => _x( 'General Settings', 'metabox tab', 'wpsso-schema-json-ld' ),
+				'types' => _x( 'Schema Types', 'metabox tab', 'wpsso-schema-json-ld' ),
 			) );
 			$table_rows = array();
 			foreach ( $tabs as $key => $title ) {
