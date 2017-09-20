4400cda620740ca2a60ff23dfd01757d2254d7ec
JS Morisset <jsm@surniaulula.com>
2017-09-20 09:18:53 -0400

    v1.16.5

--- wpsso-schema-json-ld.php
+++ wpsso-schema-json-ld.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to add Schema JSON-LD / SEO markup for Articles, Events, Local Business, Products, Recipes, Reviews + many more.
  * Requires At Least: 3.7
- * Tested Up To: 4.8.1
+ * Tested Up To: 4.8.2
  * Requires PHP: 5.3
  * Version: 1.16.5
  * 
