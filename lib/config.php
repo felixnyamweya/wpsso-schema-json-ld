4a227f044281c5813e7575b7d5c524a203f440f8
JS Morisset <jsm@surniaulula.com>
2017-11-20 07:31:21 -0600

    v1.18.3-rc.7

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssojson' => array(
-					'version' => '1.18.3-rc.6',		// plugin version
+					'version' => '1.18.3-rc.7',		// plugin version
 					'opt_version' => '10',		// increment when changing default options
 					'short' => 'WPSSO JSON',	// short plugin name
 					'name' => 'WPSSO Schema JSON-LD Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WPSSO Core',
-						'min_version' => '3.48.4-rc.6',
+						'min_version' => '3.48.4-rc.7',
 					),
 					'img' => array(
 						'icons' => array(
