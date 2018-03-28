5697120d2aeb72e0a77a1f5209bd2600336eb8f3
JS Morisset <jsm@surniaulula.com>
2018-03-28 07:51:32 -0600

    v1.24.4

--- lib/submenu/schema-json-ld.php
+++ lib/submenu/schema-json-ld.php
@@ -48,19 +48,19 @@ if ( ! class_exists( 'WpssoJsonSubmenuSchemaJsonLd' ) && class_exists( 'WpssoAdm
 
 			$table_rows = array();
 
-			foreach ( $tabs as $key => $title ) {
-				$table_rows[$key] = apply_filters( $this->p->lca.'_'.$metabox_id.'_'.$key.'_rows', 
-					$this->get_table_rows( $metabox_id, $key ), $this->form );
+			foreach ( $tabs as $tab_key => $title ) {
+				$table_rows[$tab_key] = apply_filters( $this->p->lca.'_'.$metabox_id.'_'.$tab_key.'_rows', 
+					$this->get_table_rows( $metabox_id, $tab_key ), $this->form );
 			}
 
 			$this->p->util->do_metabox_tabs( $metabox_id, $tabs, $table_rows );
 		}
 
-		protected function get_table_rows( $metabox_id, $key ) {
+		protected function get_table_rows( $metabox_id, $tab_key ) {
 
 			$table_rows = array();
 
-			switch ( $metabox_id.'-'.$key ) {
+			switch ( $metabox_id.'-'.$tab_key ) {
 
 				case 'schema_json_ld-props':
 
@@ -117,4 +117,3 @@ if ( ! class_exists( 'WpssoJsonSubmenuSchemaJsonLd' ) && class_exists( 'WpssoAdm
 		}
 	}
 }
-
