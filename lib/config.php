13ef5c381abc22eee8c64386674a5ac7f4f8acfd
JS Morisset <jsm@surniaulula.com>
2018-11-03 11:55:10 -0600

    v1.35.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoJsonConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssojson' => array(			// Plugin acronym.
-					'version'     => '1.35.0',	// Plugin version.
+					'version'     => '1.35.1',	// Plugin version.
 					'opt_version' => '17',		// Increment when changing default option values.
 					'short'       => 'WPSSO JSON',	// Short plugin name.
 					'name'        => 'WPSSO Schema JSON-LD Markup',
