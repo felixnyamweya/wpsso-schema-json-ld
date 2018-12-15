49153df7629020d438b72cd632a3f4ccaf179b1c
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:46:51 -0600

    v1.38.0

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,5 +1,8 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'Schema Markup', 'lib file description', 'wpsso-schema-json-ld' );
 _x( 'Schema Shortcode', 'lib file description', 'wpsso-schema-json-ld' );
 _x( 'WPSSO Core add-on to provide Schema JSON-LD / SEO markup for Articles, Events, Local Business, Products, Recipes, Reviews and many more.', 'plugin description', 'wpsso-schema-json-ld' );
